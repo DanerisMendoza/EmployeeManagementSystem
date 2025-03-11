b8b5dc52623d26cdf911b00b6503d2bff29ad333
Daneris Mendoza <danerismendoza096@gmail.com>
2025-03-12 06:00:22 +0800

    getAverageSalaryPerDepartment stash

--- Assignment1/src/main/java/Employee/EmployeeManagementSystem.java
+++ Assignment1/src/main/java/Employee/EmployeeManagementSystem.java
@@ -25,18 +25,33 @@ public class EmployeeManagementSystem {
     public void checkRetirementEligibility(){
         boolean anyEligible = false;
         List<Employee> employeeEligible = new ArrayList<>();
+        List<Employee> employeeNotEligible = new ArrayList<>();
         int retirementAge = 65;
         for (Employee employee: employees){
             if(employee.getAge() >= retirementAge){
                 anyEligible = true;
                 employeeEligible.add(employee);
             }
+            else{
+                employeeNotEligible.add(employee);
+            }
         }
         System.out.println("\nRetirement Eligibility:");
         System.out.println("Is anyone eligible for retirement? " + anyEligible);
         System.out.println("Employees eligible for retirement: ");
         for (Employee employee : employeeEligible){
-            System.out.println(employee.getName());
+            System.out.println("- "+employee.getName());
+        }
+        System.out.println("Employees are not eligible for retirement: ");
+        for (Employee employee : employeeNotEligible){
+            System.out.println("- "+employee.getName());
+        }
+    }
+
+    public void getAverageSalaryPerDepartment(){
+        double averageSalary, totalSalary = 0;
+        for (Employee employee : employees){
+            totalSalary += employee.getSalary();
         }
     }
 
